3a34f32ead097cefc5351cfda5cb1ad737cf23f3
Itay Ganor <6430313+itayganor@users.noreply.github.com>
2020-10-04 14:20:41 +0300

    Implemented main logic (tested)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log('hello world');
+import {useEffect, useState} from 'react';
+
+
+const CLASS_NAMES = ['pub_300x250', 'pub_300x250m', 'pub_728x90', 'text-ad', 'textAd', 'text_ad', 'text_ads', 'text-ads', 'text-ad-links'];
+
+function useAdblockDetect() {
+    const [detected, setDetected] = useState(false);
+
+    useEffect(() => {
+        const bait = document.createElement('div');
+        bait.classList.add(...CLASS_NAMES);
+        bait.style.position = 'absolute';
+        bait.style.pointerEvents = 'none';
+        bait.style.width = '1px';
+        bait.style.height = '1px';
+
+        document.body.appendChild(bait);
+
+        if (bait.offsetParent === null
+            || bait.offsetHeight == 0
+            || bait.offsetWidth == 0
+            || bait.clientHeight == 0
+            || bait.clientWidth == 0) {
+            setDetected(true);
+        } else if (window.getComputedStyle !== undefined) {
+            const baitTemp = window.getComputedStyle(bait, null);
+            if (baitTemp && (baitTemp.getPropertyValue('display') == 'none' || baitTemp.getPropertyValue('visibility') == 'hidden')) {
+                setDetected(true);
+            }
+        }
+
+        bait.remove();
+    }, []);
+
+    return detected;
+}
+
+export default useAdblockDetect;
